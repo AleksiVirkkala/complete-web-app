8b8c97f81c281ee978b4b4c807abc5da68700020
AleksiVirkkala <aleksi.virkkala@gmail.com>
2020-12-11 19:41:39 +0200

    Added import and export example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+import { formData } from './forms';
+
+const form = document.querySelector('form')!;
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const data = formData(form);
+  console.log(data);
+});
+
 console.log('Hello!');
+console.log('Hello again!');
+console.log('Hello again!');
+console.log('Hello again!');
