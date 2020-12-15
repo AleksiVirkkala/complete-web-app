90a8407d87f5951547050ee2f6cd57e346e42189
AleksiVirkkala <aleksi.virkkala@gmail.com>
2020-12-15 23:08:19 +0200

    Added util functions for managing DB connection

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,13 +1,30 @@
 import type { Server } from 'http';
+import mongoose from 'mongoose';
 
 export function createServerShutdownHandler(server: Server) {
-  return () => {
-    console.log('\nShutting down server...\n');
-    server.close(err => {
-      if (err) {
-        console.log('Closing server failed!');
-        console.error(err);
-      }
-    });
+  return async () => {
+    try {
+      // console.log('\nDisconnecting db...');
+      await mongoose.disconnect();
+      console.log('db disconnected');
+    } catch (err) {
+      console.log('Disconnecting db failed!');
+      console.error(err);
+    }
+    try {
+      // console.log('Shutting down server...');
+      await new Promise((resolve, reject) => {
+        server.close(err => (err ? reject(err) : resolve(undefined)));
+      });
+      console.log('Server closed');
+    } catch (err) {
+      console.log('Closing server failed!');
+      console.error(err);
+    }
   };
+}
+
+export async function connectDB() {
+  const DB_URL = process.env.DB_URL!;
+  await mongoose.connect(DB_URL);
 }
