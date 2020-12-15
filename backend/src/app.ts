525a7da3fe231f1d3063299b63e17ce040c70b64
AleksiVirkkala <aleksi.virkkala@gmail.com>
2020-12-15 17:31:53 +0200

    Added simple test for auto inferred content-type

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,10 @@ const app = express();
 // Listen for requests
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}...`));
 
-app.get('/', (req, res) => {
-  res.send('<p>Home Page</p>');
+app.get('/html', (req, res) => {
+  res.send('<p>HTML Page</p>');
+});
+
+app.get('/json', (req, res) => {
+  res.send({ message: 'Hello!' });
 });
