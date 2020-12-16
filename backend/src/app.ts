b6c8a618c54165b27d2b2a5dd24e19c33d40dc45
AleksiVirkkala <aleksi.virkkala@gmail.com>
2020-12-16 13:27:50 +0200

    Moved server boot to end of code for visuals

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,17 +8,6 @@ const app = express();
 // Register view engine
 app.set('view engine', 'ejs');
 
-(async () => {
-  const hostName = process.env.HOST_NAME!;
-  const port = parseInt(process.env.PORT!);
-  await connectDB();
-  // Listen for requests when db is connected
-  const server = app.listen(port, hostName, () => console.log(`Server running at: http://${hostName}:${port}`));
-  const shutdownHandler = createServerShutdownHandler(server);
-  process.on('SIGINT', shutdownHandler); // Handles ctrl + C exit
-  process.once('SIGUSR2', shutdownHandler); // Handles Nodemon restarts
-})();
-
 // Middleware & static files
 app.use(express.static('../frontend/public'));
 app.use(morgan('dev'));
@@ -46,3 +35,15 @@ app.get('/blogs/create', (req, res) => {
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });
 });
+
+// Starting server
+(async () => {
+  const hostName = process.env.HOST_NAME!;
+  const port = parseInt(process.env.PORT!);
+  await connectDB();
+  // Listen for requests once db is connected
+  const server = app.listen(port, hostName, () => console.log(`Server running at: http://${hostName}:${port}`));
+  const shutdownHandler = createServerShutdownHandler(server);
+  process.on('SIGINT', shutdownHandler); // Handles ctrl + C exit
+  process.once('SIGUSR2', shutdownHandler); // Handles Nodemon restarts
+})();
