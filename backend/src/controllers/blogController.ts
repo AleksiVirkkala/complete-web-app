6eb953f33040ba2b45185055f54455ec8cee2075
AleksiVirkkala <aleksi.virkkala@gmail.com>
2020-12-16 21:02:37 +0200

    Separated route handling logic to controller file

---
+++ backend/src/controllers/blogController.ts
@@ -0,0 +1,35 @@
+import { RequestHandler as RH } from 'express';
+import Blog from '../models/blog';
+
+const blog_index: RH = (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then(blogs => res.render('index', { title: 'All Blogs', blogs }))
+    .catch(console.log);
+};
+
+const blog_details: RH = (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then(result => res.render('details', { title: 'Blog Details', blog: result }))
+    .catch(console.log);
+};
+
+const blog_create_get: RH = (req, res) => res.render('create', { title: 'Create a new Blog' });
+
+const blog_create_post: RH = (req, res) => {
+  const newBlog = new Blog(req.body);
+  newBlog
+    .save()
+    .then(() => res.redirect('/blogs'))
+    .catch(console.log);
+};
+
+const blog_delete: RH = (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then(() => res.send({ redirect: '/blogs' }))
+    .catch(console.log);
+};
+
+export { blog_index, blog_details, blog_create_get, blog_create_post, blog_delete };
--- backend/src/routes/blogRoutes.ts
+++ backend/src/routes/blogRoutes.ts
@@ -1,36 +1,9 @@
-import express from 'express';
-import Blog from '../models/blog';
-const router = express.Router();
+import { Router } from 'express';
+import * as bc from '../controllers/blogController';
 
-router.get('/', async (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then(blogs => res.render('index', { title: 'All Blogs', blogs }))
-    .catch(console.log);
-});
-
-router.post('/', (req, res) => {
-  const newBlog = new Blog(req.body);
-  newBlog
-    .save()
-    .then(() => res.redirect('/blogs'))
-    .catch(console.log);
-});
-
-router.get('/create', (req, res) => res.render('create', { title: 'Create a new Blog' }));
-
-router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then(result => res.render('details', { title: 'Blog Details', blog: result }))
-    .catch(console.log);
-});
-
-router.delete('/:id', (req, res) => {
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-    .then(() => res.send({ redirect: '/blogs' }))
-    .catch(console.log);
-});
-
-export default router;
+export default Router()
+  .get('/', bc.blog_index)
+  .post('/', bc.blog_create_post)
+  .get('/create', bc.blog_create_get)
+  .get('/:id', bc.blog_details)
+  .delete('/:id', bc.blog_delete);
