754982edb27c98b8c88c0641f2cc82286036acd0
AleksiVirkkala <aleksi.virkkala@gmail.com>
2020-12-17 13:38:47 +0200

    Added test log to confirm receiving json data from POST request

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -3,10 +3,18 @@ import Blog from '../models/blog';
 
 const signup_get: RH = (req, res) => res.render('signup', { title: 'sign up' });
 
-const signup_post: RH = (req, res) => res.send('new signup');
+const signup_post: RH = (req, res) => {
+  const { email, password }: { [k: string]: string } = req.body;
+  console.table({ email, password });
+  res.send({ email, password });
+};
 
 const login_get: RH = (req, res) => res.render('login', { title: 'log in' });
 
-const login_post: RH = (req, res) => res.send('user login');
+const login_post: RH = (req, res) => {
+  const { email, password }: { [k: string]: string } = req.body;
+  console.table({ email, password });
+  res.send({ email, password });
+};
 
 export { signup_get, signup_post, login_get, login_post };
