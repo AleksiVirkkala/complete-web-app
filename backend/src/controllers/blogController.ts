db944f05bc2541032a75f377ecc6ccab339e5475
AleksiVirkkala <aleksi.virkkala@gmail.com>
2020-12-17 14:26:07 +0200

    Refactored to use async await

--- backend/src/controllers/blogController.ts
+++ backend/src/controllers/blogController.ts
@@ -1,38 +1,48 @@
 import { RequestHandler as RH } from 'express';
 import Blog from '../models/blog';
 
-const blog_index: RH = (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then(blogs => res.render('blogs/index', { title: 'All Blogs', blogs }))
-    .catch(() => res.render('404', { title: "Couldn't fetch blogs" }));
+const blog_index: RH = async (req, res) => {
+  try {
+    const blogs = await Blog.find().sort({ createdAt: -1 });
+    res.render('blogs/index', { title: 'All Blogs', blogs });
+  } catch {
+    res.render('404', { title: "Couldn't fetch blogs" });
+  }
 };
 
-const blog_details: RH = (req, res) => {
+const blog_details: RH = async (req, res) => {
   const id = req.params.id;
-  Blog.findById(id)
-    .then(result => {
-      if (!result) throw new Error('Blog not found');
-      res.render('blogs/details', { title: 'Blog Details', blog: result });
-    })
-    .catch(() => res.render('404', { title: 'Blog not found' }));
+  try {
+    const blog = await Blog.findById(id);
+    if (blog) return res.render('blogs/details', { title: 'Blog Details', blog });
+    throw new Error('Blog not found');
+  } catch {
+    res.render('404', { title: 'Blog not found' });
+  }
 };
 
 const blog_create_get: RH = (req, res) => res.render('blogs/create', { title: 'Create a new Blog' });
 
-const blog_create_post: RH = (req, res) => {
+const blog_create_post: RH = async (req, res) => {
+  try {
+    await Blog.create(req.body);
+    res.redirect('/blogs');
+  } catch (err) {
+    console.error(err);
+    res.status(400).send('error, blog not created');
+  }
   const newBlog = new Blog(req.body);
-  newBlog
-    .save()
-    .then(() => res.redirect('/blogs'))
-    .catch(console.log);
 };
 
-const blog_delete: RH = (req, res) => {
+const blog_delete: RH = async (req, res) => {
   const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-    .then(() => res.send({ redirect: '/blogs' }))
-    .catch(console.log);
+  try {
+    await Blog.findByIdAndDelete(id);
+    res.send({ redirect: '/blogs' });
+  } catch (err) {
+    console.error(err);
+    res.status(400).send('error, blog not deleted');
+  }
 };
 
 export { blog_index, blog_details, blog_create_get, blog_create_post, blog_delete };
