be25c8862917f6684fa4e4258d219194aa80051f
AleksiVirkkala <aleksi.virkkala@gmail.com>
2020-12-17 18:18:17 +0200

    Implemented responding with validation errors to client

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,5 +1,16 @@
 import { RequestHandler as RH } from 'express';
 import User from '../models/user';
+import { Error as mongooseError } from 'mongoose';
+
+// Handle errors
+const formatErrors = (err: Error | { code: number }) => {
+  if ('code' in err && err.code === 11000) return { email: 'email is already registered' };
+  if (err instanceof mongooseError.ValidationError) {
+    const errorEntries = Object.entries(err.errors);
+    const mappedEntries = errorEntries.map(([path, error]): [string, string] => [path, error.message]);
+    return Object.fromEntries(mappedEntries);
+  }
+};
 
 const signup_get: RH = (req, res) => res.render('signup', { title: 'sign up' });
 
@@ -9,8 +20,9 @@ const signup_post: RH = async (req, res) => {
     const newUser = await User.create({ name, email, password });
     res.status(201).json(newUser);
   } catch (err) {
-    console.log(err);
-    res.status(400).send('error, user not created');
+    const errors = formatErrors(err);
+    if (errors) return res.status(400).json(errors);
+    return res.status(400).send('error, user not created');
   }
 };
 
