c71a1c4e450917a0484fa98d40cc831e932ccda8
AleksiVirkkala <aleksi.virkkala@gmail.com>
2020-12-17 13:56:27 +0200

    Created basic signup functionality to register user to db

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,12 +1,18 @@
 import { RequestHandler as RH } from 'express';
-import Blog from '../models/blog';
+import User from '../models/user';
 
 const signup_get: RH = (req, res) => res.render('signup', { title: 'sign up' });
 
-const signup_post: RH = (req, res) => {
-  const { email, password }: { [k: string]: string } = req.body;
-  console.table({ email, password });
-  res.send({ email, password });
+const signup_post: RH = async (req, res) => {
+  const { name, email, password }: { [k: string]: string } = req.body;
+  const newUser = new User({ name, email, password });
+  try {
+    const addedUser = await newUser.save();
+    res.send(addedUser);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send('Bad request!');
+  }
 };
 
 const login_get: RH = (req, res) => res.render('login', { title: 'log in' });
