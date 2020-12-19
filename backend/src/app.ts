2159e30e30fa4c4c73d1bdff14daa3194366e0b8
AleksiVirkkala <aleksi.virkkala@gmail.com>
2020-12-19 14:28:04 +0200

    Set blog routes to require authentication

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+// Plugins
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
+// Own middleware
 import { startServer } from 'utils';
+import { requireAuth } from 'middleware/authMiddleware';
+// Routes
 import authRoutes from 'routes/authRoutes';
 import blogRoutes from 'routes/blogRoutes';
 
@@ -11,7 +15,7 @@ const app = express();
 // Register view engine
 app.set('view engine', 'ejs');
 
-// Middleware & static files
+// Middleware & static file handling
 app.use(express.static('../frontend/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,10 +26,7 @@ app.use(cookieParser());
 app.use(authRoutes);
 app.get('/', (req, res) => res.render('index', { title: 'Home' }));
 app.get('/about', (req, res) => res.render('about', { title: 'About' }));
-app.use('/blogs', blogRoutes);
+app.use('/blogs', requireAuth, blogRoutes);
+app.use((req, res) => res.status(404).render('404', { title: '404' })); // 404 page
 
-// 404 page
-app.use((req, res) => res.status(404).render('404', { title: '404' }));
-
-// Starting server
 startServer(app);
