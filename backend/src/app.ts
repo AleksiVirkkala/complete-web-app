ef2f6ce15a2832a8f062e37edca8f2735dece5cc
AleksiVirkkala <aleksi.virkkala@gmail.com>
2020-12-19 18:51:48 +0200

    Enabled user adding middleware and switched to chaining syntax

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,7 @@ import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 // Own middleware
 import { startServer } from 'utils';
-import { requireAuth } from 'middleware/authMiddleware';
+import { requireAuth, checkUser } from 'middleware/authMiddleware';
 // Routes
 import authRoutes from 'routes/authRoutes';
 import blogRoutes from 'routes/blogRoutes';
@@ -16,17 +16,20 @@ const app = express();
 app.set('view engine', 'ejs');
 
 // Middleware & static file handling
-app.use(express.static('../frontend/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
-app.use(cookieParser());
+app
+  .use(express.static('../frontend/public'))
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .use(morgan('dev'))
+  .use(cookieParser());
 
 // Routes
-app.use(authRoutes);
-app.get('/', (req, res) => res.render('index', { title: 'Home' }));
-app.get('/about', (req, res) => res.render('about', { title: 'About' }));
-app.use('/blogs', requireAuth, blogRoutes);
-app.use((req, res) => res.status(404).render('404', { title: '404' })); // 404 page
+app
+  .use(authRoutes)
+  .get('*', checkUser)
+  .get('/', (req, res) => res.render('index', { title: 'Home' }))
+  .get('/about', (req, res) => res.render('about', { title: 'About' }))
+  .use('/blogs', requireAuth, blogRoutes)
+  .use((req, res) => res.status(404).render('404', { title: '404' })); // 404 page
 
 startServer(app);
