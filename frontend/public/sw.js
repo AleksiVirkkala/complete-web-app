aca9e152c76fe9b159b527b3056cf4d33a06fb96
AleksiVirkkala <aleksi.virkkala@gmail.com>
2020-12-20 20:31:22 +0200

    Implemented route caching

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,11 +1,34 @@
+const CACHE_NAME = 'site-static';
+const ASSETS = [
+  '/',
+  '/about',
+  '/blogs',
+  '/signup',
+  '/login',
+  '/logout',
+  '/blogs/create',
+  '/js/app.js',
+  '/css/styles.css',
+  '/img/post.png',
+  '/img/trashcan.svg'
+];
 // install service worker
 self.addEventListener('install', e => {
   console.log('service worker has been installed');
-  console.log(e);
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('caching shell assets');
+      cache.addAll(ASSETS);
+    })
+  );
 });
 
 // activate service worker
 self.addEventListener('activate', e => {
   console.log('service worker has been activated');
-  console.log(e);
+});
+
+// fetch event
+self.addEventListener('fetch', e => {
+  console.log('fetch event');
 });
